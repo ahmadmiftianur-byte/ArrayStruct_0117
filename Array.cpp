e71ebcd1f6957eaf236f2b748cb9d466c87172da
ahmadmiftianur@gmail.com <ahmadmiftianur@gmail.com>
2026-04-07 18:33:49 +0700

    mengisi data melalui perulangan

--- Array.cpp
+++ Array.cpp
@@ -35,4 +35,12 @@ int main()
     cout << "data kelima: " << datamu[4] << endl;
     cout << endl;
     
-   
+    // mengisi data melalui perulangan
+ for (int i = 0; i <= 4; i++)
+    {
+        cout << "Isikan data ke-" << (i + 1) << " : ";
+        cin >> datamu[i];
+    }
+
+    cout << endl;
+    
