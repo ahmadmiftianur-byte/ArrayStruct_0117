b526070c559e67ead75019f4b4b70040a69a4555
ahmadmiftianur@gmail.com <ahmadmiftianur@gmail.com>
2026-04-07 18:36:12 +0700

    mengisi data

--- Struct.cpp
+++ Struct.cpp
@@ -13,4 +13,17 @@ int main()
     // deklarasi variable struct
     Orang mhs;
 
-    
+    // mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "Isikan data umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    cout <<"Data Mahasiswa" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
+}
