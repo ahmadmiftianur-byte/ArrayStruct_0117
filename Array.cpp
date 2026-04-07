290061a1cad0745e636198e25cfae13ba67ffff1
ahmadmiftianur@gmail.com <ahmadmiftianur@gmail.com>
2026-04-07 18:30:04 +0700

    menambahkan deklarasi array yg sdh ada dan yng belum punya

--- Array.cpp
+++ Array.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 using namespace std;
 
+int main() 
+{
+    // deklarasi array di sertai dengan inisialisasi
+    int dataku[5] = {10, 20, 30, 40, 50};
+    //deklarasi array tanpa inisialisasi
+    int datamu[5];
+
+    
